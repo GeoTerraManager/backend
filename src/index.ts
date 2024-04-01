9f8231a8c71f508920fead998ea3ddc51e974eca
otavioabreu27 <otavio.abreu96@gmail.com>
2024-03-31 21:49:05 -0300

    adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
+app.use(cors());
+
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Server running on port 3000.')
 })
